b1869e3e75efa3e84ecb68715f15b96206a985e2
Elisha <elishagjoy98@gmail.com>
2024-04-18 14:19:32 +0530

    add start date_time to the poll in scheduled polls

--- poll/signals.py
+++ poll/signals.py
@@ -7,10 +7,10 @@ import json
 @receiver(post_save, sender=ScheduledPoll)
 def create_cron_schedule_on_poll_creation(sender, instance, created, **kwargs):
     days_until_event_after_poll = instance.days_until_event_after_poll
+    poll_start_time = instance.poll_start_time
     poll_end_time = instance.poll_end_time
     event_time = instance.event_time
     poll_text = instance.poll_text
-    args = (poll_end_time, event_time, poll_text, days_until_event_after_poll)
 
     if created:
         crontab_instance = CrontabSchedule.objects.get(id=instance.crontab.id)
@@ -18,7 +18,7 @@ def create_cron_schedule_on_poll_creation(sender, instance, created, **kwargs):
             crontab=crontab_instance,
             name=instance.name,
             task='poll.tasks.create_scheduled_poll',
-            args=json.dumps([str(poll_end_time), str(event_time), poll_text, str(days_until_event_after_poll)])
+            args=json.dumps([str(poll_start_time),str(poll_end_time), str(event_time), poll_text, str(days_until_event_after_poll)])
         )
         instance.periodic_task_id = periodic_task.id
         instance.periodic_task.enable = True
