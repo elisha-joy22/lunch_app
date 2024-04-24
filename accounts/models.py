e0c13215ad9bff405e1e44cacd744f475e80baab
Elisha G Joy <elisha.joy@entri.me>
2024-04-24 10:17:31 +0000

    accounts app set up - with oauth start,redirect and profile routes working...

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class CustomUser(models.Model):
-    slack_id = models.CharField(max_length=100,unique=True, db_index=True)
+    slack_id = models.CharField(max_length=100,unique=True)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
     picture_url = models.CharField(max_length=200,blank=True)
